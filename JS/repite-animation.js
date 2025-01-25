f1d689023aa5efb1954e2d4a959f17c1088e8641
Eliezer Rosario <Eliezer2rosario@gmail.com>
2025-01-25 15:17:54 -0400

    agregue los iconos correctos de servicios

--- JS/repite-animation.js
+++ JS/repite-animation.js
@@ -1,15 +1,19 @@
-const icono = document.querySelector('#iconoAnimado');
+const iconos = document.querySelectorAll('.iconoAnimado');
 
-    // Crear el observador de intersección
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          // Forzamos el reinicio del GIF cambiando su 'src'
-          const src = icono.src;
-          icono.src = ''; // Vaciar la URL
-          icono.src = src; // Asignar nuevamente la URL
-        }
-      });
-    }, { threshold: 0.5 });
+// Crear el observador de intersección
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Forzar el reinicio del GIF cambiando su 'src'
+      const icono = entry.target;
+      const src = icono.src;
+      icono.src = ''; // Vaciar la URL
+      icono.src = src; // Asignar nuevamente la URL
+    }
+  });
+}, { threshold: 0.5 });
 
-    observer.observe(icono);
+// Observar cada ícono
+iconos.forEach(icono => {
+  observer.observe(icono);
+});
