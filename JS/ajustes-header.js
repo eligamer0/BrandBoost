230202acf3ccefcb71184376dfe5a68dfe289b44
eligamer0 <eliezer2rosario@gmail.com>
2025-04-07 02:34:17 -0400

    arreglando todas las partes de servicios 2

--- JS/ajustes-header.js
+++ JS/ajustes-header.js
@@ -1,19 +1,19 @@
-const menuInput = document.getElementById("menu-input");
-const header = document.querySelector("header");
+// const menuInput = document.getElementById("menu-input");
+// const header = document.querySelector("header");
 
-menuInput.addEventListener("change", () => {
-    if (menuInput.checked) {
-        header.style.position = "fixed"; // Fija el header
-    } else {
-        header.style.position = "relative"; // Devuelve el comportamiento normal
-    }
-});
+// menuInput.addEventListener("change", () => {
+//     if (menuInput.checked) {
+//         header.style.position = "fixed"; // Fija el header
+//     } else {
+//         header.style.position = "relative"; // Devuelve el comportamiento normal
+//     }
+// });
 
-// Ajustes del desplazamiento imnecesario del menu
-menuInput.addEventListener("change", () => {
-    if (menuInput.checked) {
-        document.body.style.overflow = "hidden";
-    } else {
-        document.body.style.overflow = "";
-    }
-});
+// // Ajustes del desplazamiento imnecesario del menu
+// menuInput.addEventListener("change", () => {
+//     if (menuInput.checked) {
+//         document.body.style.overflow = "hidden";
+//     } else {
+//         document.body.style.overflow = "";
+//     }
+// });
